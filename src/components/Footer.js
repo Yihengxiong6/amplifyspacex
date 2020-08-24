4d0b129d7d1df76cfec6ef6d7d8576c022af505a
georgexyh <xiongyiheng1996@gmail.com>
2020-08-23 22:54:52 -0400

    setup

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                (c)2020 StarLink Tracker. All Rights Reserved. Website Made by Yiheng Xiong
+            </footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const SAT_BASE_URL = "https://www.n2yo.com/rest/v1/satellite";
+
+export const SAT_API_KEY = "MY628Y-M3MM3Q-85GETH-4IZ9"; 
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = `${SAT_BASE_URL}/above`;
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = `${SAT_BASE_URL}/positions`;
